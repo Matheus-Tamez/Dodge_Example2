594c6747060b19b50e4d659e65ecab8ea089dceb
Matheus Simm Tamez <matheussimmtamez@mmc.school.nz>
2020-07-28 11:37:25 +1200

    In this version the spaceship is drawn to the panel and the planets move down randomly

---
+++ Dodge_Example2/FrmDodge.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows.Forms;
+
+namespace Dodge_Example2
+{
+    partial class FrmDodge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PnlGame = new System.Windows.Forms.Panel();
+            this.TmrPlanet = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // PnlGame
+            // 
+            this.PnlGame.BackColor = System.Drawing.Color.Silver;
+            this.PnlGame.Location = new System.Drawing.Point(43, 29);
+            this.PnlGame.Name = "PnlGame";
+            this.PnlGame.Size = new System.Drawing.Size(500, 400);
+            this.PnlGame.TabIndex = 0;
+            this.PnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlGame_Paint);
+            // 
+            // TmrPlanet
+            // 
+            this.TmrPlanet.Enabled = true;
+            this.TmrPlanet.Tick += new System.EventHandler(this.TmrPlanet_Tick);
+            // 
+            // FrmDodge
+            // 
+            this.ClientSize = new System.Drawing.Size(570, 441);
+            this.Controls.Add(this.PnlGame);
+            this.Name = "FrmDodge";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+       
+      
+        private Panel PnlGame;
+        private Timer TmrPlanet;
+    }
+}
+
